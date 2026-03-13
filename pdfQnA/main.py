78d9e8db38be9c1b42e6ab6850877faf10d812f6
piyush-1234567 <145677033+piyush-1234567@users.noreply.github.com>
2026-03-13 04:14:25 +0000

    some changes

--- pdfQnA/main.py
+++ pdfQnA/main.py
@@ -1,4 +1,20 @@
 import pymupdf
 doc =  pymupdf.open("data/basic-text.pdf")
+text = ""
 for page in doc:
-    print(page.get_text())
+    text += page.get_text()
+# print(text)
+words = text.split()
+start = 0
+overlap = 30
+chunk_size = 50
+ans = []
+i = 0
+while(start < len(words)):
+
+    chunk = words[start : start + chunk_size]
+    chunk_text = " ".join(chunk)
+    ans.append(chunk_text)
+    i = i + 1
+    start += chunk_size - overlap
+print(ans)
