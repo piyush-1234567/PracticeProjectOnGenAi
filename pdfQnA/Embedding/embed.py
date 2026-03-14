d5ef7ed74c626b1e526492fe6ab3d8c24f62e6c1
piyush-1234567 <145677033+piyush-1234567@users.noreply.github.com>
2026-03-14 05:09:03 +0000

    added the embedding module

---
+++ pdfQnA/Embedding/embed.py
@@ -0,0 +1,12 @@
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("all-MiniLM-L6-v2")
+def embedding(sentences):
+    
+    embedding = model.encode(sentences)
+    data = []
+    for text,em in zip(sentences,embedding):
+        data.append({
+            "text": text,
+            "embedding": em
+        })
+    return data
--- pdfQnA/main.py
+++ pdfQnA/main.py
@@ -1,10 +1,11 @@
-from extractorF.extractor import extract_text
+from PdfextractorF.extractor import extract_text
 from chunking.makeChunks import make_Chunks
+from Embedding.embed import embedding
 def main():
     pdf_path = "./data/basic-text.pdf"
     text = extract_text(pdf_path)
     chunks = make_Chunks(text)
-    print(chunks)
-
+    ans = embedding(chunks)
+    print(ans)
 if __name__ == "__main__":
     main()
