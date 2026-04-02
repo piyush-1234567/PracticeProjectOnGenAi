66073e635952f5b876ecaa811d6d7cee72dd8c3f
Piyush Jain <piyushjain07032004@gmail.com>
2026-04-02 23:26:47 +0530

    break the main function in two segments process_pdf and answer_query

--- StudySage/pdfQnA/main.py
+++ StudySage/pdfQnA/main.py
@@ -4,27 +4,37 @@ from Embedding.embed import embedding
 from vectorDb.vector import vectorDbStorage
 from prompt.prompt import promptBuilder
 from response.responses import ask_llm
-def main():
-    pdf_path = "./data/basic-text.pdf"
+
+def process_pdf(pdf_path):
     text = extract_text(pdf_path)
     chunks = make_Chunks(text)
-    ans = embedding(chunks)
-    hello = vectorDbStorage(ans)
+    embeddings = embedding(chunks)
+    vector_store = vectorDbStorage(embeddings)
+    return vector_store
     # query = "what formatting options are available in the document?"
-    while(True):
-        query = input("how can i help you today ? ")
-        if query == "e":
-            break
-        query_embedding = embedding([query])[0]["embedding"]
-        results = hello.query(
-            query_embeddings = query_embedding,
-            n_results = 3
-        )
-        chunks = results['documents'][0]
-        context = '\n'.join(chunks)
+def answer_query(query,vector_store):
 
-        prompt = promptBuilder(context,query)
-        ll = ask_llm(prompt)
-        print(ll)
+    query_embedding = embedding([query])[0]["embedding"]
+    results = vector_store.query(
+        query_embeddings = query_embedding,
+        n_results = 3
+    )
+    chunks = results['documents'][0]
+    context = '\n'.join(chunks)
+
+    prompt = promptBuilder(context,query)
+    answer = ask_llm(prompt)
+    return answer
+
+def main():
+    vector_store = process_pdf("./data/basic-text.pdf")
+    while True:
+        ask = input("Ask: ")
+        if( ask == "e"):
+            break
+        answer = answer_query(ask,vector_store)
+        print(answer)
+
+    return "hello"  
 if __name__ == "__main__":
     main()
